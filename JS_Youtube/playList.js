4ff91f1288d8f98248ff104b6f8631bd8af09458
wenw1001 <wenw1001@wenwenlin-MacBook-Pro.local>
2021-06-25 23:25:39 +0800

    final

---
+++ JS_Youtube/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "SwZMS-ISbH4",
+    "AwwbhhjQ9Xk",
+    "c6gi6IP6ay4"
+];
+//播放起訖秒數 
+let playTime = [
+    [101,105],
+    [18,24],
+    [146,152]
+];
---
+++ JS_Youtube/playList2.js
@@ -0,0 +1,14 @@
+//YouTube Video ID
+let playList = [
+    "M2FJJ7vSGoY",
+    "M2FJJ7vSGoY",
+    "M2FJJ7vSGoY",
+    "AwwbhhjQ9Xk"
+];
+//播放起訖秒數 
+let playTime = [
+    [4,12],
+    [43,47],
+    [64,72],
+    [55,68]
+];
